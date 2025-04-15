3d86effa323b0bb2edc6ff93e468e8253295ffea
s1ldqz <sldforwork1@gmail.com>
2025-04-15 10:38:10 +0300

    Images update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-const headerMenuBtn = document.querySelector(".header__menu");
-const headerSubmenu = document.querySelector(".header__submenu");
-
-headerMenuBtn.addEventListener("click", () => {
-  headerSubmenu.classList.toggle("header__submenu--active");
+document.addEventListener("click", (e) => {
+  const headerMenuBtn = e.target.matches(".header__menu");
+  const headerSubmenu = document.querySelector(".header__submenu");
+  const activeSubMenu = document.querySelector(
+    ".header__submenu.header__submenu--active"
+  );
+  if (headerMenuBtn) {
+    headerSubmenu.classList.toggle("header__submenu--active");
+  }
+  if (activeSubMenu) {
+    headerSubmenu.classList.remove("header__submenu--active");
+  }
 });
